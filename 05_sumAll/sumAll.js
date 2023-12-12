1e0e732ddcc8ec4240c1853e1d70954151764577
Arjun <imarjunsiv@gmail.com>
2023-12-12 10:01:57 +0530

    05 completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(fnum, snum) {
+    if(typeof(fnum) !='number' || typeof(snum)!="number" || fnum*snum < 0){
+        return "ERROR"
+    }
+    if(fnum > snum){
+        return sumAll(snum,fnum);
+    }
+    let sum = 0;
+    for(let i = fnum; i<=snum; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
