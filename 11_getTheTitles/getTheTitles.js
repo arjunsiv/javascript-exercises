b7e1fdee1c9adc88789dbde96cc7e3e6002544ae
Arjun <imarjunsiv@gmail.com>
2023-12-14 00:27:47 +0530

    11 12 completed

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    return array.map((object)=>{
+        return object["title"];
+    })
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldestObject,currentobject) =>{
+        let oldestAge;
+        if(!("yearOfDeath" in oldestObject)){
+            oldestAge = new Date().getFullYear() - oldestObject.yearOfBirth;
+        }
+        else{
+            oldestAge = oldestObject.yearOfDeath - oldestObject.yearOfBirth;
+        }
+        let age = 0;
+        if(!("yearOfDeath" in currentobject)){
+            age = new Date().getFullYear() - currentobject.yearOfBirth;
+        }
+        else{
+            age = currentobject.yearOfDeath - currentobject.yearOfBirth;
+        }
+        if(age > oldestAge){
+            oldestObject = currentobject;
+        }
+        return oldestObject;
+    },people[0]);
 };
 
 // Do not edit below this line
