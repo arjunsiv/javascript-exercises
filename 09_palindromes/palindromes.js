b018969a015e9469dd77ffb1c380eb232261a99f
Arjun <imarjunsiv@gmail.com>
2023-12-13 22:15:42 +0530

    08 and 09 done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
 
+
+const palindromes = function (string) {
+    let i = 0;
+    let j = string.length-1;
+    while(i<j){
+        if(string[i]=="!" || string[i] =="," || string[i] == "." || string[i] ==" "){
+            i++;
+            continue;
+        }
+        else if (string[j]=="!" || string[j] =="," || string[j] == "." || string[j] ==" "){
+            j--;
+            continue;
+        }
+        if(string[i].toLowerCase() != string[j].toLowerCase()){
+            return false;
+        }
+        i++;
+        j--;
+    }
+    return true;
 };
 
 // Do not edit below this line
